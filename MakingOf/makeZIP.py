732c4de94883257dcef5cf9cad11f23fb20467cd
Cyrille <cyrille.guieu@gmail.com>
2019-06-04 16:54:34 -0300

    modified:   ../FreqLeq.zip 	modified:   Data/freqlex.dat 	modified:   FreqLex.py 	deleted:    Lexique381.txt 	deleted:    base_lexique.py 	modified:   makeZIP.py

--- MakingOf/makeZIP.py
+++ MakingOf/makeZIP.py
@@ -3,8 +3,13 @@ from zipfile import ZipFile
 
 
 archive=ZipFile("../FreqLeq.zip","w")
-archive.write("FreqLex.py")
+archive.write("Scripts/FreqLex_core.py")
+archive.write("Scripts/base_lexique.py")
+archive.write("Scripts/gui_freqlex.py")
+archive.write("Scripts/licence_freqlex.py")
 archive.write("Data/freqlex.dat")
+archive.write("Data/icon.ico")
+archive.write("FreqLex.py")
 archive.close()
 
 
